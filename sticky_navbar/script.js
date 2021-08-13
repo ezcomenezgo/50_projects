e212a17b54878cd0a9bdd181b10af6d93505800d
Claire <s3310507057@gmail.com>
2021-08-13 11:08:50 +0800

    project init

---
+++ sticky_navbar/script.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-08-13 10:41:55
+ * @LastEditTime: 2021-08-13 10:41:55
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /_project_starter/script.js
+ */
